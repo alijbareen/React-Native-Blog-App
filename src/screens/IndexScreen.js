c8caf20ce5c97c373bbd0135b5d3c7b04b2b3b68
alijb <alijb@post.bgu.ac.il>
2020-09-24 23:36:58 +0300

    Initializing the index screen and adding App Component

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const IndexScreen = () => {
+  return (
+    <View>
+      <Text>IndexScreen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default IndexScreen;
